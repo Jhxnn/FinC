8c439f2a405d26a34a96a1c6476db9339bb99b58
Hilixx <jhonalizan@gmail.com>
2025-03-29 09:56:17 -0300

    aa

---
+++ src/main/java/com/FinC/FinCApplication.java
@@ -0,0 +1,13 @@
+package com.FinC;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinCApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinCApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/FinC/FinCApplicationTests.java
@@ -0,0 +1,13 @@
+package com.FinC;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinCApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
